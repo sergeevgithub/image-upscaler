b16e950124f32dfde68c1a2ba4aa094ec4f2b1a8
Mikhail Sergeev <45151687+sergeevgithub@users.noreply.github.com>
2025-01-16 00:42:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,20 +12,20 @@ if uploaded_file is not None:
     input_image = Image.open(uploaded_file).convert("RGB")
     st.image(input_image, caption="Uploaded Image", use_container_width=True)
 
-    # # Process the image
-    # with st.spinner("Upscaling..."):
-    #     upscaled_image = input_image
+    # Process the image
+    with st.spinner("Upscaling..."):
+        upscaled_image = input_image
 
-    # # Display the upscaled image
-    # st.image(upscaled_image, caption="Upscaled Image", use_container_width=True)
+    # Display the upscaled image
+    st.image(upscaled_image, caption="Upscaled Image", use_container_width=True)
 
-    # # Option to download the upscaled image
-    # st.markdown("### Download the Upscaled Image")
-    # upscaled_image.save("upscaled_image.png")
-    # with open("upscaled_image.png", "rb") as file:
-    #     btn = st.download_button(
-    #         label="Download Image",
-    #         data=file,
-    #         file_name="upscaled_image.png",
-    #         mime="image/png"
-    #     )
+    # Option to download the upscaled image
+    st.markdown("### Download the Upscaled Image")
+    upscaled_image.save("upscaled_image.png")
+    with open("upscaled_image.png", "rb") as file:
+        btn = st.download_button(
+            label="Download Image",
+            data=file,
+            file_name="upscaled_image.png",
+            mime="image/png"
+        )
